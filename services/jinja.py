ee6dc95e639d63befeb2058eb1a7e6dee63a1915
fcgith <steliyan.slavov31@icloud.com>
2025-05-11 09:41:00 +0300

    Fixed templates folder path

--- services/jinja.py
+++ services/jinja.py
@@ -1,7 +1,5 @@
 from pathlib import Path
-
 from fastapi.templating import Jinja2Templates
 
-templates_dir = Path("../templates")
-templates_dir.mkdir(exist_ok=True)
+templates_dir = Path(__file__).parent.parent / "templates"
 templates = Jinja2Templates(directory=templates_dir)
